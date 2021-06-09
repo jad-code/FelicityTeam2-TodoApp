ddcb3b7ea81a155f8602c90a87cd038ce013d196
snigdhamajeti02 <85132285+snigdhamajeti02@users.noreply.github.com>
2021-06-09 17:36:50 -0400

    Adding HomePage

---
+++ flutter_application_1/lib/main.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: HomePage(),
+  ));
+}
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        title: Center(
+          child: Text(
+            "ECSTATIC",
+            style: TextStyle(
+              color: Colors.black,
+              fontFamily: 'Rounded Mplus',
+              fontSize: 20,
+            ),
+          ),
+        ),
+        bottom: PreferredSize(
+            child: Row(
+              children: <Widget>[
+                Expanded(
+                  child: Container(
+                    height: 5.0,
+                    color: Colors.black,
+                  ),
+                ),
+              ],
+            ),
+            preferredSize: Size.square(1.0)),
+      ),
+    );
+  }
+}
